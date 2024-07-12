ef92ad217197afb80ed4c0c023e9e45ae0f8c0de
enchic <alwayswithya@gmail.com>
2024-07-12 11:59:51 +0900

    VitePress index.html 임시 포함

--- sample-docs/.vitepress/vitepress-pdf.config.ts
+++ sample-docs/.vitepress/vitepress-pdf.config.ts
@@ -2,27 +2,28 @@ import { defineUserConfig } from 'vitepress-export-pdf'
 
 const routeOrder = [
   '/cover.html',
+  '/index.html',
   '/markdown-examples.html',
   '/api-examples.html',
   // ...
 ];
 
 export default defineUserConfig({
-  routePatterns: ['!/index.html'],
-    outDir: './sample-docs/dist-pdf',
-    pdfOptions: {
-        format: 'A4',
-        printBackground: true,
-        displayHeaderFooter: true,
-        // headerTemplate,
-        // footerTemplate,
-        /* margin: {
-            bottom: 60,
-            left: 25,
-            right: 25,
-            top: 60,
-        }, */
-    },
+  // routePatterns: ['!/index.html'],
+  outDir: './sample-docs/dist-pdf',
+  pdfOptions: {
+    format: 'A4',
+    printBackground: true,
+    displayHeaderFooter: true,
+    // headerTemplate,
+    // footerTemplate,
+    /* margin: {
+        bottom: 60,
+        left: 25,
+        right: 25,
+        top: 60,
+    }, */
+  },
   sorter: (pageA, pageB) => {
     const aIndex = routeOrder.findIndex(route => route === pageA.path)
     const bIndex = routeOrder.findIndex(route => route === pageB.path)
